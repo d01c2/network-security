aa12750cf6898ae0db3960581f826137526087a9
Hyunjoon Kim <rickykhj@korea.ac.kr>
2024-03-17 13:11:03 +0900

    Refactored skeleton

--- main.go
+++ main.go
@@ -9,10 +9,7 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-var (
-	intf   string = os.Args[1]
-	BUFSIZ int32  = 512
-)
+const BUFSIZ int32 = 512
 
 func main() {
 	if len(os.Args) != 2 {
@@ -20,6 +17,8 @@ func main() {
 		fmt.Printf("sample: pcap-test wlan0\n")
 		panic("Invalid usage")
 	}
+
+	var intf string = os.Args[1]
 
 	if handle, err := pcap.OpenLive(intf, BUFSIZ, true, time.Second); err != nil {
 		fmt.Printf("pcap_open_live(%s) return null\n", intf)
